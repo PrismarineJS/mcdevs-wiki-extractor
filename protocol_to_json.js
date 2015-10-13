57faadb04fa5af942cb9b06afed70205b8cf6dda
Romain Beaumont <romain.rom1@gmail.com>
2015-10-13 20:47:08 +0200

    fix usage

--- protocol_to_json.js
+++ protocol_to_json.js
@@ -1,5 +1,5 @@
 if(process.argv.length !=3) {
-  console.log("Usage : node protocol_extractor.js <protocol.json>");
+  console.log("Usage : node protocol_to_json.js <protocol.json>");
   process.exit(1);
 }
 var protocolFilePath=process.argv[2];
